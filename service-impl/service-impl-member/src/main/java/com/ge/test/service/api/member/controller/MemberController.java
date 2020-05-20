699774531adf79ec0cb01ad6c1cc64226aff9cf5
P-peng <894194651@qq.com>
2020-05-21 00:42:35 +0800

    分布式配置中心没搞好

--- service-impl/service-impl-member/src/main/java/com/ge/test/service/api/member/controller/MemberController.java
+++ service-impl/service-impl-member/src/main/java/com/ge/test/service/api/member/controller/MemberController.java
@@ -2,6 +2,8 @@ package com.ge.test.service.api.member.controller;
 
 import com.ge.test.service.api.member.impl.MemberServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,13 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2020/5/20 0020
  */
 @RestController
+@RefreshScope
 public class MemberController {
 
     @Autowired
     MemberServiceImpl memberService;
+//
+//    @Value("${test.name}")
+//    private String configName;
 
     @GetMapping("/get")
     public Object get(){
         return memberService.getUser(2);
     }
+
+//    @GetMapping("/getConfig")
+//    public Object getConfig(){
+//        return configName;
+//    }
 }
