fa62c8efaae819e0f123f8e2c47bf00203ddd1a5
P-peng <894194651@qq.com>
2020-05-21 22:23:03 +0800

    测试提交

--- service-impl/service-impl-member/src/main/java/com/ge/test/service/api/MemberMain.java
+++ service-impl/service-impl-member/src/main/java/com/ge/test/service/api/MemberMain.java
@@ -11,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MemberMain {
 
+
     public static void main(String[] args) {
         SpringApplication.run(MemberMain.class);
     }
